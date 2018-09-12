aac4b9dd3cba3071483a041914ed9772e096064e
arianaberger <ariana.berger@gmail.com>
2018-09-12 16:23:43 -0400

    added logout functionality on user's show page

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+  post "/logout", to: "sessions#destroy"
 
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
