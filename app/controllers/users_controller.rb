7e6770c521a328188bdef94e6b0367719c9d3cb4
arianaberger <ariana.berger@gmail.com>
2018-09-12 16:30:59 -0400

    added some validations for user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.valid?
+      @user.save
       session[:user_id] = @user.id
       redirect_to dashboard_path(@user)
     else
-      redirect_to signup_path
+      redirect_to signup_path #add error message to form if fields are missing
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   has_many :projects
   has_many :clients, through: :projects
   has_secure_password
-  
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+
+
 end
