ad8c9c252658225e8922bd74b546db97754e5852
arianaberger <ariana.berger@gmail.com>
2018-09-12 16:19:59 -0400

    added sign up functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,24 @@
 class UsersController < ApplicationController
 
-  def new
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to dashboard_path(@user)
+    else
+      redirect_to signup_path
+    end
   end
 
-  def create
+  def show
+    @user = User.find(session[:user_id])
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :title, :email, :password, :password_confirmation)
+  end
+
 
 end
