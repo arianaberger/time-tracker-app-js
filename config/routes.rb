116321ce204cc9c8df2e7a8ac61b1d45fae9abb6
arianaberger <ariana.berger@gmail.com>
2018-09-12 15:38:42 -0400

    added routes including nested ones

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :sessions, only: [:new, :create, :destroy]
+  root "welcome#home"
 
+  get "/signin", to: "sessions#new"
+  post "/sessions/create", to: "sessions#create"
+  delete "/signout", to: "sessions#destroy"
+
+  resources :users
+
+  resources :clients do
+    resources :projects, only: [:index]
+  end
+
+  resources :projects do
+    resources :entries, shallow: true
+  end
 
 end
