6277fe62ea00eb39a579feeb057ec83f4be16f53
arianaberger <ariana.berger@gmail.com>
2018-09-13 20:41:09 -0400

    finalized new client form and added client show page

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,6 +4,24 @@ class ClientsController < ApplicationController
   end
 
   def create
+    @client = Client.new(client_params)
+    if @client.valid?
+      @client.save
+      redirect_to client_path(@client)
+    else
+      #add error to say 'name must be filled out'?
+      render :new
+    end
+  end
+
+  def show
+    @client = Client.find(params[:id])
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:name)
   end
 
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,6 @@ class Client < ApplicationRecord
   has_many :projects
   has_many :users, through: :projects
 
+  validates :name, presence: true
+
 end
