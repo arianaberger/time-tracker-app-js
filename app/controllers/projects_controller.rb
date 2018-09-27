1bbf25a36542d8d1975fba951f572ae5f5f44c67
arianaberger <ariana.berger@gmail.com>
2018-09-27 18:47:47 -0400

    added partial form for edit/new project and updated projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,15 +13,10 @@ class ProjectsController < ApplicationController
   end
 
   def new
-    if params[:client_id]
-      @name = Client.find(params[:client_id]) 
-    else
-      @name = User.find(session[:user_id])
-    end
+    @project = Project.new
   end
 
   def create
-    # raise params.inspect
     @project = Project.new(project_params)
       if @project.valid?
         @project.save
@@ -35,6 +30,16 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @client = @project.client
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+    redirect_to @project
   end
 
   private
