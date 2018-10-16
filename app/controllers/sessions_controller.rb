15200278ed1a4a7adc931ac5b073df2de1d11b6d
arianaberger <ariana.berger@gmail.com>
2018-10-16 16:12:40 -0400

    added facebook login omniauth code to sessions controller and .env file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,37 @@
 class SessionsController < ApplicationController
 
   def create
-    user = User.find_by(email: params[:user][:email])
-    if user && user.authenticate(params[:user][:password])
-      session[:user_id] = user.id
-      redirect_to '/dashboard'
+    if auth_hash
+      @user = User.find_or_create_by(uid: auth_hash['uid']) do |u|
+        u.name= auth_hash['info']['name']
+        u.email= auth_hash['info']['email']
+        u.password= SecureRandom.hex
+      end
+      session[:user_id] = @user.id
+      redirect_to @user
     else
-      flash[:notice] = "Please make sure your email and password are entered correctly."
-      redirect_to '/login'
+      user = User.find_by(email: params[:user][:email])
+
+      if user && user.authenticate(params[:user][:password])
+        session[:user_id] = user.id
+        redirect_to '/dashboard'
+      else
+        flash[:notice] = "Please make sure your email and password are entered correctly."
+        redirect_to '/login'
+      end
     end
   end
 
   def destroy
-    session[:user_id] = nil
+    session.delete :user_id
     flash[:notice] = "You have successfully logged out!"
     redirect_to '/'
+  end
+
+  private
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 
 end
