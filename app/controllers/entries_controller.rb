9cddc65c7b16e0c506d6d5b0292043f6eb1d9441
arianaberger <ariana.berger@gmail.com>
2018-10-22 15:47:37 -0400

    added delete function for entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -14,6 +14,11 @@ class EntriesController < ApplicationController
 		redirect_to entries_path
 	end
 
+	def destroy
+		Entry.find(params[:id]).destroy
+		redirect_to entries_path
+	end
+
 	private
 
 	def entry_params
