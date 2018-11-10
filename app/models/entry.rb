76c2a7ee7fd7e50bacfdf30bee8d935bedb9429c
arianaberger <ariana.berger@gmail.com>
2018-11-09 19:10:20 -0500

    trying to create a calc hours method to sum all hours for a project or user

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -11,10 +11,22 @@ class Entry < ApplicationRecord
    end
 
    def calc_time(time)
+   	raise inspect	
 	time.strftime("%H:%M")    
    end
 
-   def calc_hours
+   def calc_hours(user_or_project)
+   	#get the entries.all for each user/project 
+   	#iterate over each and get start and end times 
+   	#convert each using calc_time and then subtract to get the total time
+   	#collect in an array and add together array.sum
+   	#add error when creating new time entry that it has to be on the same day??
+
+   	time_array = array.new
+   	user_or_project.entries.each do |entry|
+   		time_array << calc_time(entry.start_time).slice[0,1].to_i - calc_time(entry.end_time).slice[0,1].to_i
+   	end
+   	time_array.sum
    end
 
 
