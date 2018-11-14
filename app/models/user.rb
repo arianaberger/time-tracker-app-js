7ea512c12acb467d2855ffee737b503a6576aad8
arianaberger <ariana.berger@gmail.com>
2018-11-14 17:00:53 -0500

    added validation to email so it's proper format

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true 
   validates :password, presence: true
   validates :password_confirmation, presence: true
 
-
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
 end
