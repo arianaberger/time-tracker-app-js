d4aec67559294977ee3bbce85958d59aef7907ee
arianaberger <ariana.berger@gmail.com>
2018-11-15 16:23:30 -0500

    more css styling, mostly with nav bars

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  post "/logout", to: "sessions#destroy"
+  post "/logout", to: "sessions#destroy", as: '/logout'
 
   get '/signup' => 'users#new', as: 'signup'
   post '/signup' => 'users#create'
