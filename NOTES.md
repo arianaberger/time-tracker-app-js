9a0d0986b172cda17988a72e0d7c67e5851d0de5
arianaberger <ariana.berger@gmail.com>
2019-02-28 20:46:39 -0500

    add working ajax request for entries

--- NOTES.md
+++ NOTES.md
@@ -1,14 +1,9 @@
-ToDo: 
-
-Remove turbolinks
-	- remove gem
-	- check <head> tags
-	- manifest file (should have //= require_tree .)
-
+- entries json data only shows id for project and user, how to display?
 
 28.02
 - copied app on github
 - removed turbolinks gem/from js manifest
+- added jquery gem and proper require to JS manifest file
 
 
 
--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
 	console.log('JS is working!');
+
 	//execute function to display data when page is done loading
-})
+	loadEntries();
+});
+
+function loadEntries() {
+	$.ajax({
+		url: 'http://localhost:3000/entries',
+		method: 'get',
+		dataType: 'json',
+	}).done(function (data) {
+		console.log("the data is", data);
+	});
+};
