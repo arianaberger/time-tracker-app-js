d2b24884659bc5c2016541e8ac8034278e4336ce
arianaberger <ariana.berger@gmail.com>
2019-02-28 20:52:18 -0500

    create entry constructor

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -12,5 +12,17 @@ function loadEntries() {
 		dataType: 'json',
 	}).done(function (data) {
 		console.log("the data is", data);
+		let entry = new Entry(data[0]);
+		debugger
 	});
 };
+
+class Entry {
+	constructor(obj) {
+		this.start_time = obj.start_time
+		this.end_time = obj.end_time
+		this.notes = obj.notes
+		this.project = obj.project.name
+		this.user = obj.user.name
+	}
+}
