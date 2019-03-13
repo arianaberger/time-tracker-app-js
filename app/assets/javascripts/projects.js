fecea9ca58745af514267e9fc105cc21d0072df5
arianaberger <ariana.berger@gmail.com>
2019-03-12 20:01:33 -0400

    add new project form dynamically to project page with JS

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -6,6 +6,64 @@ $(document).ready(function () {
 });
 
 function listenerNewProject() {
-	document.getElementById('js-add-project')
-	debugger
+	$('a#js-add-project').on('click', function(event) {
+		event.preventDefault();
+		let newProjectFormHTML = newProjectForm();
+		// debugger
+		document.getElementById('js-new-project-form').innerHTML = newProjectFormHTML;
+	})
+}
+
+function newProjectForm() {
+	let HTML = `
+		<hr>
+		<h3>Add New Project</h3>
+		<form>
+	  <table>
+	    <tr>
+	      <td>
+	        Project Owner: 
+	      </td>
+	      <td>
+	        <select>
+			  <option value="kitty">Kitty</option>
+			  <option value="karl">Karl Lagerfeld</option>
+			  <option value="chanel">Coco Chanel</option>
+			</select>
+	      </td>
+	    </tr>
+	      <td>
+	        Project Name: 
+	      </td>
+	      <td>
+	        <input id="project-name" type='text' name='name'>
+	      </td>
+	    <tr>
+	      <td>
+	        Deadline:
+	      </td>
+	      <td>
+	        <input id="project-deadline" type='date' name='deadline'>
+	      </td>
+	    </tr>
+	    <tr>
+	      <td>
+	        Status:
+	      </td>
+	      <td>
+	        <select>
+			  <option value="just-started">Just Started</option>
+			  <option value="in-progress">In Progress</option>
+			  <option value="almost-finished">Almost Finished</option>
+			  <option value="completed">Complete</option>
+	  		  <option value="total-failure">Total Failure</option>
+			  <option value="losing-money">Losing Money</option>
+			</select>
+	      </td>
+        </tr>
+	  </table>
+	    <p><input type="submit" class="btn btn-primary btn-sm" %></p>
+	</form>
+	`
+	return HTML;
 }
