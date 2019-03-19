2097369a8bf4b0d74bcc9ac45e5407b5788cffc3
arianaberger <ariana.berger@gmail.com>
2019-03-18 20:03:31 -0400

    create project js model and format date

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -12,5 +12,24 @@ function listenerDisplayShowPageData() {
 		dataType: 'json',
 	}).done(function (data) {
 		console.log('project data is:', data)
+		let project = new Project(data)
 	})
+}
+
+class Project {
+	constructor(obj) {
+		this.name = obj.project.name
+		this.status = obj.project.status
+		this.deadline = formatDeadline(obj.project.deadline)
+	}
+}
+
+function formatDeadline(p) {
+	let projectDeadline = new Date(p);
+	let year = projectDeadline.getFullYear();
+	let months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	let month = months[projectDeadline.getMonth()];
+	let day = projectDeadline.getDate();
+	let date = month + " " + day + ", " + year 
+	return date;
 }
